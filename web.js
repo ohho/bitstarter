ac9708931aa8dce1ed4f8cd36d3ce1b73c1adf7d
Ubuntu <ubuntu@ip-172-31-15-175.us-west-2.compute.internal>
2013-07-22 03:19:14 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,15 +2,15 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
-var buffer = new Buffer (100);
+var buffer = new Buffer ('test');
 
-fs.writeFile('index.html', buffer, function (err) {
+/*fs.writeFile('index.html', buffer, function (err) {
 	if (err) throw err;
 	console.log('It\'s saved!');
 });
-
+*/
 app.get('/', function(request, response) {
-  response.send('buffer');
+  response.send(buffer);
 });
 
 var port = process.env.PORT || 5000;
